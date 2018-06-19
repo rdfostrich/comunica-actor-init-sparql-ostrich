53976afbaa65854041316ef56d4b345d633705f2
Ruben Taelman <ruben.taelman@ugent.be>
2018-06-19 14:12:19 +0200

    Fix failing inDev mode in binary

--- bin/run.ts
+++ bin/run.ts
@@ -1,3 +1,5 @@
 #!/usr/bin/env node
 import {runArgsInProcess} from "@comunica/runner-cli";
-runArgsInProcess(__dirname + '/../', __dirname + '/../config/config-default.json');
+import {existsSync} from "fs";
+const inDev: boolean = existsSync(__dirname + '/run.ts');
+runArgsInProcess(__dirname + '/../', __dirname + '/../config/config-default.json', inDev);

3550c59ec6c39ccde693fb438bf3c773e68e31aa
Ruben Taelman <ruben.taelman@ugent.be>
2018-10-04 12:29:27 +0200

    Update to comunica 1.4.0

--- bin/run.ts
+++ bin/run.ts
@@ -1,5 +1,3 @@
 #!/usr/bin/env node
 import {runArgsInProcess} from "@comunica/runner-cli";
-import {existsSync} from "fs";
-const inDev: boolean = existsSync(__dirname + '/run.ts');
-runArgsInProcess(__dirname + '/../', __dirname + '/../config/config-default.json', inDev);
+runArgsInProcess(__dirname + '/../', __dirname + '/../config/config-default.json');
